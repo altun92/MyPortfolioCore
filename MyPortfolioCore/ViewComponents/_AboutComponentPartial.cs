ef59da2b312227a4b1cefc8a7a7fb2c10f94490a
Anıl <Anl@ANL>
2024-12-05 09:56:05 +0300

    Add project files.

---
+++ MyPortfolioCore/ViewComponents/_AboutComponentPartial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioCore.DAL.Context;
+
+namespace MyPortfolioCore.ViewComponents
+{
+    public class _AboutComponentPartial : ViewComponent
+    {
+        MyPortfolioContext portfolioContext = new MyPortfolioContext();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.aboutTitle = portfolioContext.Abouts.Select(x => x.Title).FirstOrDefault();
+            ViewBag.subDescription = portfolioContext.Abouts.Select(x => x.SubDescription).FirstOrDefault();
+            ViewBag.detail = portfolioContext.Abouts.Select(x => x.Detail).FirstOrDefault();
+            return View();
+        }
+    }
+}
